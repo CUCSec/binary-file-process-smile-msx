018322dd278c8b15a52ab92a027358d18bd672c8
smile-msx <50402661+smile-msx@users.noreply.github.com>
2019-07-03 13:27:10 +0800

    Add files via upload

---
+++ 马淑贤2.py
@@ -0,0 +1,53 @@
+import struct
+import os
+
+def Fun1(student_id):
+  name=os.path.abspath(__file__)
+  lname=os.path.dirname(name)
+  full_name=os.path.join(lname,"lenna.bmp")
+  f=open("lenna.bmp", "r+b")
+  f.seek(54+2*3)
+  a=bytes([0,0,0])
+  f.write(a)
+  for l in range(1,12):
+      flag=int(student_id[l])
+      if flag==0:
+          flag=10
+      f.seek((flag-1)*3,1)
+      a=bytes([0,0,0])
+      f.write(a)
+  f.close()
+
+
+def Fun2():
+  with open('lenna.bmp', 'rb') as f:
+    bmp_file_header = f.read(14)
+
+    bm, size, r1, r2, offset = struct.unpack('<2sIHHI', bmp_file_header)
+
+    f.seek(offset)
+
+    count = 12
+    offset = 0
+    last_offset = 0
+    while count > 0:
+      color = f.read(3)
+
+      if color == b'\x00\x00\x00':
+
+        if offset - last_offset == 10:
+          print(0)
+        else:
+          print(offset - last_offset)
+
+        last_offset = offset
+        count -= 1
+
+      offset += 1
+
+
+if __name__ == '__main__':
+  import sys
+  Fun1(sys.argv[1])
+
+  detect()
